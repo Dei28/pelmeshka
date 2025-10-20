5d49ec5fe7e346970024ee080d552b6add7784c3
Dei28 <Brawl-Wezent2@yandex.ru>
2025-10-20 11:13:20 +0300

    Add files via upload

---
+++ papka1/самостоятельная работа/data.js
@@ -0,0 +1,24 @@
+const dataProducts = `
+[
+    {
+       "name": "MANGO  PEOPLE  T-SHIRT",
+       "url": "картинка1.jpg",
+       "price": "$300",
+       "color": "Red",
+       "size": "Xl"
+    },
+    {
+       "name": "ELLERY X M'O CAPSULE",
+       "url": "картинка2.jpg",
+       "price": "$52",
+       "color": "Black",
+       "size": "Xl"
+    },
+      {
+       "name": "MANGO  PEOPLE  T-SHIRT",
+       "url": "картинка1.jpg",
+       "price": "$250",
+       "color": "Blue",
+       "size": "XXl"
+    }
+]`
