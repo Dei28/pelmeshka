6218e65b10619e9dcd99ba3268f3290c1598905d
Dei28 <Brawl-Wezent2@yandex.ru>
2025-10-20 12:34:07 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const dataProducts =`
+[
+    {
+        "name":"mango people t-shrift",
+        "url":"Rectangle 15 copy (1).jpg",
+        "price":"300",
+        "color":"red",
+        "size":"XL"
+    },
+     {
+        "name":"ELLERY X M'O CAPSULE",
+        "url":"Rectangle 15 copy.jpg",
+        "price":"52",
+        "color":"BLACK",
+        "size":"XL"
+    }
+]
+`
